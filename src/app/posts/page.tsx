955a75a18c55fc8c49b4d98ab41cdbc5207471b3
soohyn <sooheyonjo@gmail.com>
2024-09-11 15:13:14 +0900

    feat: get search params on posts page

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,6 +1,20 @@
+"use client";
 import { NextPage } from "next";
+import { ReadonlyURLSearchParams, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const Posts: NextPage = () => {
-  return <div>posts</div>;
+  const [search, setSearch] = useState<string | null>("");
+  const params = useSearchParams();
+
+  const getSearch = (params: ReadonlyURLSearchParams) => {
+    setSearch(params.get("search"));
+  };
+
+  useEffect(() => {
+    getSearch(params);
+  }, [params]);
+
+  return <div>posts {search}</div>;
 };
 export default Posts;
