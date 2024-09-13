c0266f3cc828089006b9c3eec6a3613810ee3ac2
soohyn <sooheyonjo@gmail.com>
2024-09-13 10:06:28 +0900

    feat: home profile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
+import Contacts from "@/components/Contacts";
+import Profile from "@/components/Profile";
 import { NextPage } from "next";
 
-const Home:NextPage =()=>{
-  return <div>Home</div>
-}
+const Home: NextPage = () => {
+  return (
+    <div className="py-10 flex flex-col  gap-20">
+      <section className="flex flex-col items-center gap-2 select-none">
+        <Profile />
 
-export default Home
+        <div className="p-4">
+          <div className="h-0.5 w-4 bg-gray-100 rounded-full"></div>
+        </div>
+        <Contacts />
+      </section>
+
+      <section className="flex flex-col mt-10 "></section>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,31 @@
+import { url } from "@/lib";
+import Image from "next/image";
+import { FC } from "react";
+
+const Profile: FC = () => {
+  return (
+    <a
+      className="flex flex-col items-center gap-6 hover:opacity-50 active:scale-95 transition-all ease-in-out"
+      target="_blank"
+      href={url.portfolio}
+    >
+      <div className="relative">
+        <div className="w-36 h-36 overflow-hidden border-4 rounded-full border-gray-100 ">
+          <Image src={""} alt="soohyn" fill={true} />
+        </div>
+        <div className="bg-white absolute bottom-1 right-1 flex justify-center text-2xl items-center right-4 w-10 h-10 overflow-hidden border-4 rounded-full border-gray-100">
+          <span>🎁</span>
+        </div>
+      </div>
+
+      <div className="text-center">
+        <h1 className="font-extrabold text-2xl">Soohyn</h1>
+        <span className="font-semibold text-gray-500 text-lg">
+          Frontend developer
+        </span>
+      </div>
+    </a>
+  );
+};
+
+export default Profile;
